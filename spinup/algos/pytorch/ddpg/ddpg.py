089d88fba1a7d227ce299fb8908b423a46b17a26
Sanjeevan Ahilan <sanjeevanahilan@gmail.com>
2020-04-21 21:10:29 +0100

    decentralised ddpg

--- spinup/algos/pytorch/ddpg/ddpg.py
+++ spinup/algos/pytorch/ddpg/ddpg.py
@@ -309,8 +309,8 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
-    parser.add_argument('--hid', type=int, default=256)
+    parser.add_argument('--env', type=str, default='Pendulum-v0')
+    parser.add_argument('--hid', type=int, default=32)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
---
+++ spinup/algos/pytorch/dec_ddpg/core.py
@@ -0,0 +1,148 @@
+import numpy as np
+import scipy.signal
+from copy import deepcopy
+from torch.optim import Adam
+import torch
+import torch.nn as nn
+
+
+def combined_shape(length, shape=None):
+    if shape is None:
+        return (length,)
+    return (length, shape) if np.isscalar(shape) else (length, *shape)
+
+def mlp(sizes, activation, output_activation=nn.Identity):
+    layers = []
+    for j in range(len(sizes)-1):
+        act = activation if j < len(sizes)-2 else output_activation
+        layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
+    return nn.Sequential(*layers)
+
+def count_vars(module):
+    return sum([np.prod(p.shape) for p in module.parameters()])
+
+class MLPActor(nn.Module):
+
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
+        super().__init__()
+        pi_sizes = [obs_dim] + list(hidden_sizes) + [act_dim]
+        self.pi = mlp(pi_sizes, activation, nn.Tanh)
+        self.act_limit = act_limit
+
+    def forward(self, obs):
+        # Return output from network scaled to action space limits.
+        return self.act_limit * self.pi(obs)
+        # return self.pi(obs)*torch.Tensor(self.act_limit)
+
+class MLPQFunction(nn.Module):
+
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        self.q = mlp([obs_dim + act_dim] + list(hidden_sizes) + [1], activation)
+
+    def forward(self, obs, act):
+        q = self.q(torch.cat([obs, act], dim=-1))
+        return torch.squeeze(q, -1) # Critical to ensure q has right shape.
+
+class MLPActorCritic(nn.Module):
+
+    def __init__(self, obs_dim, act_dim, act_limit, hidden_sizes=(256,256),
+                 activation=nn.ReLU):
+        super().__init__()
+
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.act_limit = act_limit
+
+        # build policy and value functions
+        self.pi = MLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit)
+        self.q = MLPQFunction(obs_dim, act_dim, hidden_sizes, activation)
+
+    def act(self, obs):
+        with torch.no_grad():
+            return self.pi(obs).numpy()
+            # print([self.pi(obs)*a for a in self.act_limit])
+            # return [self.pi(obs)*a for a in self.act_limit][0].numpy()
+
+class DDPG:
+    def __init__(self, ac, pi_lr, q_lr, gamma, polyak, logger):
+        self.ac = ac
+        self.ac_targ = deepcopy(ac)
+
+        # Freeze target networks with respect to optimizers (only update via polyak averaging)
+        for p in self.ac_targ.parameters():
+            p.requires_grad = False
+
+        # Count variables (protip: try to get a feel for how different size networks behave!)
+        var_counts = tuple(count_vars(module) for module in [ac.pi, ac.q])
+        logger.log('\nNumber of parameters: \t pi: %d, \t q: %d\n' % var_counts)
+
+        # Set up optimizers for policy and q-function
+        self.pi_optimizer = Adam(ac.pi.parameters(), lr=pi_lr)
+        self.q_optimizer = Adam(ac.q.parameters(), lr=q_lr)
+        self.gamma = gamma
+        self.polyak = polyak
+        self.logger = logger
+
+    # Set up function for computing DDPG Q-loss
+    def compute_loss_q(self, data):
+        o, a, r, o2, d = data['obs'], data['act'], data['rew'], data['obs2'], data['done']
+
+        q = self.ac.q(o, a)
+
+        # Bellman backup for Q function
+        with torch.no_grad():
+            q_pi_targ = self.ac_targ.q(o2, self.ac_targ.pi(o2))
+            backup = r + self.gamma * (1 - d) * q_pi_targ
+
+        # MSE loss against Bellman backup
+        loss_q = ((q - backup) ** 2).mean()
+
+        # Useful info for logging
+        loss_info = dict(QVals=q.detach().numpy())
+
+        return loss_q, loss_info
+
+    # Set up function for computing DDPG pi loss
+    def compute_loss_pi(self, data):
+        o = data['obs']
+        q_pi = self.ac.q(o, self.ac.pi(o))
+        return -q_pi.mean()
+
+    def update(self, data):
+        # First run one gradient descent step for Q.
+        self.q_optimizer.zero_grad()
+        loss_q, loss_info = self.compute_loss_q(data)
+        loss_q.backward()
+        self.q_optimizer.step()
+
+        # Freeze Q-network so you don't waste computational effort
+        # computing gradients for it during the policy learning step.
+        for p in self.ac.q.parameters():
+            p.requires_grad = False
+
+        # Next run one gradient descent step for pi.
+        self.pi_optimizer.zero_grad()
+        loss_pi = self.compute_loss_pi(data)
+        loss_pi.backward()
+        self.pi_optimizer.step()
+
+        # Unfreeze Q-network so you can optimize it at next DDPG step.
+        for p in self.ac.q.parameters():
+            p.requires_grad = True
+
+        # Record things
+        self.logger.store(LossQ=loss_q.item(), LossPi=loss_pi.item(), **loss_info)
+
+        # Finally, update target networks by polyak averaging.
+        with torch.no_grad():
+            for p, p_targ in zip(self.ac.parameters(), self.ac_targ.parameters()):
+                # NB: We use an in-place operations "mul_", "add_" to update target
+                # params, as opposed to "mul" and "add", which would make new tensors.
+                p_targ.data.mul_(self.polyak)
+                p_targ.data.add_((1 - self.polyak) * p.data)
+
+    def get_action(self, o, noise_scale):
+        a = self.ac.act(torch.as_tensor(o, dtype=torch.float32))
+        a += noise_scale * np.random.randn(self.ac.act_dim)
+        return np.clip(a, -self.ac.act_limit, self.ac.act_limit)
